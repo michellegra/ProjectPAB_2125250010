697d41b3826808621d697d3696e391532bdfd448
michellegra <mchllgrcl@gmail.com>
2023-06-03 16:19:28 +0700

    Update detail lom sesai

--- app/src/main/java/com/if4a/projectpab/Activity/DetailActivity.java
+++ app/src/main/java/com/if4a/projectpab/Activity/DetailActivity.java
@@ -2,6 +2,7 @@ package com.if4a.projectpab.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,16 +10,30 @@ import android.widget.TextView;
 import com.if4a.projectpab.R;
 
 public class DetailActivity extends AppCompatActivity {
-    public static final String ITEM_EXTRA = "item_extra";
-
+    private TextView  tvNama, tvRasa,tvRating,tvHarga,tvDeskripsiSingkat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView imgjajanan = findViewById(R.id.img);
-        TextView tvDetailnama = findViewById(R.id.tv_detailnama);
-        TextView tvDetailRasa = findViewById(R.id.tv_detailrasa);
+        tvNama=findViewById(R.id.tv_detailnama);
+        tvRasa=findViewById(R.id.tv_detailrasa);
+        tvRating=findViewById(R.id.tv_detailrating);
+        tvHarga=findViewById(R.id.tv_detailharga);
+        tvDeskripsiSingkat=findViewById(R.id.tv_detaildeskripsiSingkat);
+
+        Intent intent = getIntent();
+        String nama = intent.getStringExtra("varNama");
+        String rasa = intent.getStringExtra("varRasa");
+        String rating = intent.getStringExtra("varRating");
+        String harga = intent.getStringExtra("varHarga");
+        String deskripsiSingkat = intent.getStringExtra("varDeskripsiSingkat");
+
+        tvNama.setText(nama);
+        tvRasa.setText(rasa);
+        tvRating.setText(rating);
+        tvHarga.setText(harga);
+        tvDeskripsiSingkat.setText(deskripsiSingkat);
 
 
 
